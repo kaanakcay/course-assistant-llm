fdc4c909d89345a990c4b9295f014042501c8965
Ömer Can Öztürk <ozturkomer@sabanciuniv.edu>
2024-05-01 20:59:03 +0300

    Erased unnecessary labels after login

--- src/app/page.js
+++ src/app/page.js
@@ -18,20 +18,20 @@ export default async function Home() {
         <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
             <div className="flex flex-col items-center text-center">
                 <div className="flex items-center">
-                    <h1 className="mr-3 text-5xl font-semibold">Sabanci University Course Assistant</h1>
+                    {!isAuth && (<h1 className="mr-3 text-5xl font-semibold">Sabanci University Course Assistant</h1>)}       
                     <UserButton afterSignOutUrl="/" />
                 </div>    
 
                 <div className="flex mt-2">
-                    {isAuth && (
+                    {!isAuth && (
                         <Button>Chat with AssistantSU</Button>
                     )}
 
                 </div>
-                <p className="max-w-xl mt-1 text-lg text-slate-600">
+                {!isAuth && ( <p className="max-w-xl mt-1 text-lg text-slate-600">
                     Join millions of students, researchers and professionals to instantly
                     answer questions and understand research with AI
-                </p>
+                </p>)}   
 
                 <div className="w-full mt-4">
                     {!isAuth ?
@@ -44,9 +44,6 @@ export default async function Home() {
                      : (<div className="w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100">
                      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
                          <div className="flex flex-col items-center text-center">
-                             <div className="flex items-center">
-                             <h1 className="mr-3 text-5xl font-semibold">Sabanci University Course Assistant</h1>
-                             </div>
                              <CombinedChat/>
                          </div>
                      </div>
